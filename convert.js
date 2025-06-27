c0c8748d7b80d32ff180a6c69610054386a656a0
Anaslrhazouli <anaslrh2@outlook.com>
2025-06-27 11:18:49 +0200

    Add root route to serve HTML interface

--- convert.js
+++ convert.js
@@ -2,11 +2,20 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors());
+
+// Serve static files (like index.html)
+app.use(express.static(__dirname));
+
+// Root route - serve the HTML interface
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Pure math functions for unit testing
 function convert(amount, fromCurrency, toCurrency) {
